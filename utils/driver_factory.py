6855e30c2a35841720be0911ca8a065403142312
Dmitry Pakhomov <dmitry.pakhomov@americor.com>
2025-06-27 17:29:22 +0300

    delete unusual files

--- utils/driver_factory.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-def get_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--start-maximized')
-    return webdriver.Chrome(options=options)
